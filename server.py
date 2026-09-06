471ddbfd3e1388c013753abc9884011cb0df233c
root <rglove02@uoguelph.ca>
2026-09-06 20:43:43 +0000

    Fixing indent error

--- server.py
+++ server.py
@@ -285,11 +285,11 @@ class MyHandler( BaseHTTPRequestHandler ):
     if resetTableID is not None:
         tableIDList.append(resetTableID)
 
-      self.send_response(200)
-      self.send_header('Content-Type', 'application/json')
-      self.end_headers()
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
 
-      self.wfile.write(
+        self.wfile.write(
           json.dumps({
               "status": "success",
               "tableSVG": tableSVG
